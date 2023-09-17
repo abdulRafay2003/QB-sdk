2359beb4379a949df7259a4c16053457394b5caa
TECH VERSATILE <techversatile2020@gmail.com>
2023-09-18 03:17:46 +0500

    initial commit

---
+++ webrtc-sample/src/QBConfig.js
@@ -0,0 +1,17 @@
+// export default {
+//   accountKey: "fKUzVUSq2j8s_FJJxouy",
+//   apiEndpoint: '',
+//   appId: "96909",
+//   authKey:"xbXFCKDeZ3gur-G",
+//   authSecret: "ED-6cNvmCrJKdqG",
+//   chatEndpoint: '',
+// };
+
+export default {
+  appId: '96909',
+  authKey: 'xbXFCKDeZ3gur-G',
+  authSecret: 'ED-6cNvmCrJKdqG',
+  accountKey: 'fKUzVUSq2j8s_FJJxouy',
+  apiEndpoint: '',
+  chatEndpoint: '',
+};
